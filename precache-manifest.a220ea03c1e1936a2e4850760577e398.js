e8b9a33bddd16a1efb60ee637341ddd56ab989a8
vitio <vitio@mymailbox.com>
2020-07-09 00:01:56 +0300

    Updates

---
+++ precache-manifest.a220ea03c1e1936a2e4850760577e398.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d90481bdc6a0aa12b34407298e64fc",
+    "url": "/funny-tiles/index.html"
+  },
+  {
+    "revision": "d5d23b7992fb6c07672a",
+    "url": "/funny-tiles/static/css/main.72014e9e.chunk.css"
+  },
+  {
+    "revision": "5ce037a652a765c3aa04",
+    "url": "/funny-tiles/static/js/2.794d6c15.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/funny-tiles/static/js/2.794d6c15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5d23b7992fb6c07672a",
+    "url": "/funny-tiles/static/js/main.a772a20b.chunk.js"
+  },
+  {
+    "revision": "0890ad39f19f4cb19fe7",
+    "url": "/funny-tiles/static/js/runtime-main.aa1bc679.js"
+  }
+]);
